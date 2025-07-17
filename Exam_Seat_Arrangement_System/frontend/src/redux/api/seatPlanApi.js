0b96f0d90df0248384292cc2ba255a8acbe95840
Neesui <neeshachy2020@gmail.com>
2025-07-17 20:25:15 +0545

    refactor(seatPlanApi): remove redundant comments from API endpoints
    The comments above the API endpoint definitions were redundant since the function names are self-descriptive. This improves code cleanliness and maintainability.

--- Exam_Seat_Arrangement_System/frontend/src/redux/api/seatPlanApi.js
+++ Exam_Seat_Arrangement_System/frontend/src/redux/api/seatPlanApi.js
@@ -2,7 +2,6 @@ import { apiSlice } from "./apiSlice";
 
 export const seatPlanApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    // Generate seating plan
     generateSeatingPlan: builder.mutation({
       query: (examId) => ({
         url: `/api/seating/generate/${examId}`,
@@ -11,7 +10,6 @@ export const seatPlanApi = apiSlice.injectEndpoints({
       invalidatesTags: ["SEAT_PLAN"],
     }),
 
-    // Get all seating plans
     getAllSeatingPlans: builder.query({
       query: () => "/api/seating/all",
       providesTags: ["SEAT_PLAN"],
