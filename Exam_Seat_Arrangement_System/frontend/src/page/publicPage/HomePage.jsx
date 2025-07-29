f6d89ecea54f1d68b8d3c1eaaa6fc20eb9f112c1
Neesui <neeshachy2020@gmail.com>
2025-07-29 22:40:44 +0545

    feat(homepage): add hero component with image and call-to-action

--- Exam_Seat_Arrangement_System/frontend/src/page/publicPage/HomePage.jsx
+++ Exam_Seat_Arrangement_System/frontend/src/page/publicPage/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../../component/public/homepage/Hero'
 
 const HomePage = () => {
   return (
-    <div>NISHA</div>
+    <>
+    <Hero />
+    </>
   )
 }
 
