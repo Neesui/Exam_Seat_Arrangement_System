ed9d1ad3a20bb21a669d711b82d97b906deecbd6
Neesui <neeshachy2020@gmail.com>
2025-10-03 23:57:07 +0545

    Add new public routes for viewing exam rooms and seat plans

--- Exam_Seat_Arrangement_System/frontend/src/router/routes/PublicRoutes.jsx
+++ Exam_Seat_Arrangement_System/frontend/src/router/routes/PublicRoutes.jsx
@@ -1,11 +1,22 @@
 
 import HomePage from "../../pages/publicPage/HomePage";
+import ViewExamRoomPage from "../../pages/publicPage/ViewExamRoomPage";
+import ViewSeatPlanPage from "../../pages/publicPage/ViewSeatPlanPage";
 
 export const publicRoutes = [
   {
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "/exam_room",
+    element: <ViewExamRoomPage />,
+  },
+  {
+    path: "/viewSeatPlan",
+    element: <ViewSeatPlanPage />,
+  }
+
   
 
 ];
