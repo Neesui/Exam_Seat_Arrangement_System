e7e471616faaaf0acae9a04f3a82019bc1e7e928
Neesui <neeshachy2020@gmail.com>
2025-10-06 14:43:17 +0545

    feat(invigilator): Add new invigilator routes for profile management and room assignment viewing

--- Exam_Seat_Arrangement_System/frontend/src/router/routes/InvigilatorRoutes.jsx
+++ Exam_Seat_Arrangement_System/frontend/src/router/routes/InvigilatorRoutes.jsx
@@ -1,9 +1,34 @@
 import InvigilatorDashboard from "../../pages/invigilatorPage/InvigilatorDashboard";
+import ViewInvigilatorAssignmentPage from "../../pages/invigilatorPage/ViewInvigilatorAssignmentPage";
+import ViewRoomAssignDetailsPage from "../../pages/adminPages/ViewRoomAssignDetailsPage";
+import ViewRoomAssignPage from "../../pages/adminPages/ViewRoomAssignPage";
+import InvigilatorProfilePage from "../../pages/invigilatorPage/InvigilatorProfilePage";
+import EditInvigilatorProfilePage from "../../pages/invigilatorPage/EditInvigilatorProfilePage";
 
  const invigilatorRoutes = [
     {
       path: '/invigilator',
       element: <InvigilatorDashboard />
+    },
+    {
+      path: '/invigilator/profile',
+      element: <InvigilatorProfilePage />
+    },
+    {
+      path: '/invigilator/update-profile',
+      element: <EditInvigilatorProfilePage />
+    },
+    // {
+    //   path: '/invigilator/viewRoomAssign',
+    //   element: <ViewInvigilatorAssignmentPage />
+    // },
+    {
+      path: "/invigilator/viewRoomAssign",
+      element: <ViewRoomAssignPage />,
+    },
+    {
+      path: "/invigilator/viewRoomAssignDetails/:examId",
+      element: <ViewRoomAssignDetailsPage />,
     },
   ];
 
